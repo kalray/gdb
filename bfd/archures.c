0b7e2c61157b4420331c949d58bc8cab6d7351d4
Hudson <hudson@jupiter.lin.mbt.kalray.eu>
2010-08-30 18:59:05 +0200

    Update gdb with changes in GBU files

--- bfd/archures.c
+++ bfd/archures.c
@@ -433,7 +433,7 @@ DESCRIPTION
 .#define bfd_mach_st230                2
 .#define bfd_mach_st231                3
 .#define bfd_mach_st240                4
-.  bfd_arch_k1,        {* KALRAY k1 *}
+.  bfd_arch_k1,        {* Kalray k1 *}
 .#define bfd_mach_k1v1                 0
 .#define bfd_mach_k1dp                 1
 .#define bfd_mach_k1cp                 2
