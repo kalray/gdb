4763c5634f7825f01be1c48ccbaafcb9e57b2e45
Hudson <hudson@jupiter.lin.mbt.kalray.eu>
2010-09-21 18:16:59 +0200

    Update gdb with changes in GBU files

--- opcodes/k1-dis.c
+++ opcodes/k1-dis.c
@@ -168,7 +168,8 @@ int print_insn_k1 (bfd_vma memaddr, struct disassemble_info *info){
       break;
     default:
       /* Core not supported */
-      (*info->fprintf_func)(info->stream, "disassembling not supported for this K1 core!");
+      (*info->fprintf_func)(info->stream, "disassembling not supported for this K1 core! (core:%d)",
+			    info->mach);
       return -1;
   }
 
