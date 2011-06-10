0cf07d8ce6da0c4c1af816d53567b83706c80404
Benoit Dinechin <benoit.dinechin@kalray.eu>
2011-06-10 13:04:06 +0200

    Sync with gbu [aci/bd3/master 7d87abcfc7398ea4a6e7df4533ff4ef61debcc8e].

--- bfd/elf32-k1.c
+++ bfd/elf32-k1.c
@@ -10,9 +10,9 @@ static reloc_howto_type* k1_reloc_type_lookup (bfd *, bfd_reloc_code_real_type);
 static reloc_howto_type* k1_reloc_name_lookup (bfd *, const char *);
 static void k1_elf_info_to_howto (bfd *, arelent *, Elf_Internal_Rela *);
 
-#define K1V1_K1DP
+#define K1DP_K1V1
 #include "elf32-k1.def"
-#undef K1V1_K1DP
+#undef K1DP_K1V1
 
 struct k1_reloc_map
 {
