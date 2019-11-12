9eb3ab0a84bb4831cccebf25baea827b0e651dc3
Marius Gligor <mgligor@kalray.eu>
2019-11-12 11:10:31 +0100

    Pass all Kalray source files to GNU coding style
    Summary:
    In order to have an uniform coding style, all Kalray specific files
    have been passed to the GNU coding style
    
    Test Plan: gdb testsuite
    
    Reviewers: O12 gdb!
    
    Differential Revision: https://phab.kalray.eu/D2388

--- gdb/k1-common-tdep.c
+++ gdb/k1-common-tdep.c
@@ -31,7 +31,7 @@
 #include "opcode/k1c.h"
 #include "k1-common-tdep.h"
 
-#define __NR_BREAKPOINT_PL0     4050
+#define __NR_BREAKPOINT_PL0 4050
 #define __NR_BREAKPOINT_JTAGISS 4054
 
 struct k1_frame_cache
@@ -59,24 +59,24 @@ k1_arch (void)
     return k1_current_arch;
 
   if (exec_bfd)
-  {
-    switch (elf_elfheader (exec_bfd)->e_flags & ELF_K1_CORE_MASK)
     {
-      case ELF_K1_CORE_C_C:
-        k1_current_arch = K1_K1C;
-        break;
-      default:
-        error (_ ("The K1 binary is compiled for an unknown core."));
+      switch (elf_elfheader (exec_bfd)->e_flags & ELF_K1_CORE_MASK)
+	{
+	case ELF_K1_CORE_C_C:
+	  k1_current_arch = K1_K1C;
+	  break;
+	default:
+	  error (_ ("The K1 binary is compiled for an unknown core."));
+	}
     }
-  }
   else if (desc && tdesc_architecture (desc))
-  {
-    const char *name = tdesc_architecture (desc)->printable_name;
-    if (!strncmp (name, "k1:k1c", 6))
-      k1_current_arch = K1_K1C;
-    else
-      error ("unable to find the current k1 architecture.");
-  }
+    {
+      const char *name = tdesc_architecture (desc)->printable_name;
+      if (!strncmp (name, "k1:k1c", 6))
+	k1_current_arch = K1_K1C;
+      else
+	error ("unable to find the current k1 architecture.");
+    }
 
   if (k1_current_arch == K1_NUM_ARCHES)
     return K1_K1C;
@@ -106,17 +106,19 @@ k1_adjust_breakpoint_address (struct gdbarch *gdbarch, CORE_ADDR bpaddr)
   int i = 0;
 
   adjusted &= ~3;
-  if (adjusted == 0 || ptid_equal (inferior_ptid, null_ptid) || !is_stopped (inferior_ptid))
+  if (adjusted == 0 || ptid_equal (inferior_ptid, null_ptid)
+      || !is_stopped (inferior_ptid))
     return adjusted;
 
   /* Look for the end of the bundle preceeding the requested address. */
   do
-  {
-    if (target_read_memory (adjusted - 4, syllab_buf, 4) != 0)
-      return adjusted;
+    {
+      if (target_read_memory (adjusted - 4, syllab_buf, 4) != 0)
+	return adjusted;
 
-    syllab = extract_unsigned_integer (syllab_buf, 4, order);
-  } while ((i++ < 7) && (syllab >> 31) && (adjusted -= 4));
+      syllab = extract_unsigned_integer (syllab_buf, 4, order);
+    }
+  while ((i++ < 7) && (syllab >> 31) && (adjusted -= 4));
 
   if (i == 8) /* Impossible for correct instructions */
     return bpaddr & ~3;
@@ -125,8 +127,9 @@ k1_adjust_breakpoint_address (struct gdbarch *gdbarch, CORE_ADDR bpaddr)
 }
 
 /* Return PC of first real instruction.  */
-/* This function is nearly completely copied from symtab.c:skip_prologue_using_lineinfo
- * (expect the test in the loop at the end.
+/* This function is nearly completely copied from
+ * symtab.c:skip_prologue_using_lineinfo (expect the test in the loop at the
+ * end.
  */
 CORE_ADDR
 k1_skip_prologue (struct gdbarch *gdbarch, CORE_ADDR func_addr)
@@ -160,34 +163,34 @@ k1_skip_prologue (struct gdbarch *gdbarch, CORE_ADDR func_addr)
      entry whose PC is in the range [FUNC_START..FUNC_END[ is the
      address we are looking for.  */
   if (gcc_compiled)
-  {
-    ind = 0;
-    for (i = 0; i < l->nitems; i++)
     {
-      struct linetable_entry *item = &(l->item[i]);
+      ind = 0;
+      for (i = 0; i < l->nitems; i++)
+	{
+	  struct linetable_entry *item = &(l->item[i]);
 
-      if (item->line > 0 && func_start <= item->pc && item->pc < func_end)
-      {
-        if (ind == 0)
-          ++ind;
-        else
-          return item->pc;
-      }
+	  if (item->line > 0 && func_start <= item->pc && item->pc < func_end)
+	    {
+	      if (ind == 0)
+		++ind;
+	      else
+		return item->pc;
+	    }
+	}
     }
-  }
   else
-  {
-    for (i = 0; i < l->nitems; i++)
     {
-      struct linetable_entry *item = &(l->item[i]);
+      for (i = 0; i < l->nitems; i++)
+	{
+	  struct linetable_entry *item = &(l->item[i]);
 
-      /* Don't use line numbers of zero, they mark special entries in
-        the table.  See the commentary on symtab.h before the
-        definition of struct linetable.  */
-      if (item->line > 0 && func_start < item->pc && item->pc < func_end)
-        return item->pc;
+	  /* Don't use line numbers of zero, they mark special entries in
+	    the table.  See the commentary on symtab.h before the
+	    definition of struct linetable.  */
+	  if (item->line > 0 && func_start < item->pc && item->pc < func_end)
+	    return item->pc;
+	}
     }
-  }
 
   return func_addr;
 }
@@ -195,50 +198,52 @@ k1_skip_prologue (struct gdbarch *gdbarch, CORE_ADDR func_addr)
 CORE_ADDR
 k1_unwind_pc (struct gdbarch *gdbarch, struct frame_info *next_frame)
 {
-  return frame_unwind_register_unsigned (next_frame, gdbarch_pc_regnum (gdbarch));
+  return frame_unwind_register_unsigned (next_frame,
+					 gdbarch_pc_regnum (gdbarch));
 }
 
 /* Given a GDB frame, determine the address of the calling function's
    frame.  This will be used to create a new GDB frame struct.  */
 static void
-k1_frame_this_id (struct frame_info *this_frame, void **this_prologue_cache, struct frame_id *this_id)
+k1_frame_this_id (struct frame_info *this_frame, void **this_prologue_cache,
+		  struct frame_id *this_id)
 {
   if (get_frame_func (this_frame) == get_frame_pc (this_frame))
-    *this_id = frame_id_build (get_frame_sp (this_frame), get_frame_func (this_frame));
+    *this_id
+      = frame_id_build (get_frame_sp (this_frame), get_frame_func (this_frame));
 }
 
 /* Get the value of register regnum in the previous stack frame.  */
 static struct value *
 k1_frame_prev_register (struct frame_info *this_frame,
-  void **this_prologue_cache, int regnum)
+			void **this_prologue_cache, int regnum)
 {
   if (get_frame_func (this_frame) == get_frame_pc (this_frame))
-  {
-    if (regnum == gdbarch_pc_regnum (get_frame_arch (this_frame)))
     {
-      return frame_unwind_got_register (this_frame, regnum,
-        user_reg_map_name_to_regnum (get_frame_arch (this_frame), "ra", -1));
+      if (regnum == gdbarch_pc_regnum (get_frame_arch (this_frame)))
+	{
+	  return frame_unwind_got_register (
+	    this_frame, regnum,
+	    user_reg_map_name_to_regnum (get_frame_arch (this_frame), "ra",
+					 -1));
+	}
+      return frame_unwind_got_register (this_frame, regnum, regnum);
     }
-    return frame_unwind_got_register (this_frame, regnum, regnum);
-  }
 
   return frame_unwind_got_optimized (this_frame, regnum);
 }
 
-const struct frame_unwind k1_frame_unwind =
-{
-  NORMAL_FRAME,
-  default_frame_unwind_stop_reason,
-  k1_frame_this_id,
-  k1_frame_prev_register,
-  NULL,
-  default_frame_sniffer
-};
+const struct frame_unwind k1_frame_unwind = {NORMAL_FRAME,
+					     default_frame_unwind_stop_reason,
+					     k1_frame_this_id,
+					     k1_frame_prev_register,
+					     NULL,
+					     default_frame_sniffer};
 
 void
 k1_dwarf2_frame_init_reg (struct gdbarch *gdbarch, int regnum,
-  struct dwarf2_frame_state_reg *reg,
-  struct frame_info *this_frame)
+			  struct dwarf2_frame_state_reg *reg,
+			  struct frame_info *this_frame)
 {
   if (regnum == gdbarch_pc_regnum (gdbarch))
     reg->how = DWARF2_FRAME_REG_RA;
@@ -261,10 +266,10 @@ k1_print_insn (bfd_vma pc, disassemble_info *di)
      bundle.  This is of course a lie, but it manages what ze zant
      to achieve: print the full bundle when invoking 'x/i $pc'.  */
   if (buf[3] & 0x80)
-  {
-    di->insn_info_valid = 1;
-    di->branch_delay_insns = 1;
-  }
+    {
+      di->insn_info_valid = 1;
+      di->branch_delay_insns = 1;
+    }
 
   return res;
 }
@@ -281,7 +286,8 @@ k1_dummy_id (struct gdbarch *gdbarch, struct frame_info *this_frame)
   CORE_ADDR sp;
   struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
 
-  // force read all registers to avoid writing all registers when returning the dummy call
+  // force read all registers to avoid writing all registers when returning the
+  // dummy call
   get_frame_register_unsigned (this_frame, tdep->r0_regnum + 20);
 
   sp = get_frame_register_unsigned (this_frame, gdbarch_sp_regnum (gdbarch));
@@ -289,43 +295,50 @@ k1_dummy_id (struct gdbarch *gdbarch, struct frame_info *this_frame)
 }
 
 CORE_ADDR
-k1_push_dummy_call (struct gdbarch *gdbarch, struct value *function, struct regcache *regcache,
-  CORE_ADDR bp_addr, int nargs, struct value **args, CORE_ADDR sp, int struct_return, CORE_ADDR struct_addr)
+k1_push_dummy_call (struct gdbarch *gdbarch, struct value *function,
+		    struct regcache *regcache, CORE_ADDR bp_addr, int nargs,
+		    struct value **args, CORE_ADDR sp, int struct_return,
+		    CORE_ADDR struct_addr)
 {
   enum bfd_endian byte_order = gdbarch_byte_order (gdbarch);
   struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
   gdb_byte *argslotsbuf = NULL;
   unsigned int argslotsnb = 0;
   int i, len;
-  int r0_regnum = user_reg_map_name_to_regnum (get_regcache_arch (regcache), "r0", -1);
+  int r0_regnum
+    = user_reg_map_name_to_regnum (get_regcache_arch (regcache), "r0", -1);
   int gpr_reg_sz = register_size (gdbarch, 0);
   const int params_regs = 12;
 
   /* Allocate arguments to the virtual argument slots  */
   for (i = 0; i < nargs; i++)
-  {
-    int typelen = TYPE_LENGTH (value_enclosing_type (args[i]));
-    int newslots = (typelen + gpr_reg_sz - 1) / gpr_reg_sz;
+    {
+      int typelen = TYPE_LENGTH (value_enclosing_type (args[i]));
+      int newslots = (typelen + gpr_reg_sz - 1) / gpr_reg_sz;
 
-    if (typelen > gpr_reg_sz && argslotsnb & 1)
-      ++argslotsnb;
+      if (typelen > gpr_reg_sz && argslotsnb & 1)
+	++argslotsnb;
 
-    argslotsbuf = xrealloc (argslotsbuf, (argslotsnb + newslots) * gpr_reg_sz);
-    memset (&argslotsbuf[argslotsnb * gpr_reg_sz], 0, newslots * gpr_reg_sz);
-    memcpy (&argslotsbuf[argslotsnb * gpr_reg_sz], value_contents (args[i]), typelen);
-    argslotsnb += newslots;
-  }
+      argslotsbuf
+	= xrealloc (argslotsbuf, (argslotsnb + newslots) * gpr_reg_sz);
+      memset (&argslotsbuf[argslotsnb * gpr_reg_sz], 0, newslots * gpr_reg_sz);
+      memcpy (&argslotsbuf[argslotsnb * gpr_reg_sz], value_contents (args[i]),
+	      typelen);
+      argslotsnb += newslots;
+    }
 
   for (i = 0; i < (argslotsnb > params_regs ? params_regs : argslotsnb); i++)
-    regcache_cooked_write (regcache, i + r0_regnum, &argslotsbuf[i * gpr_reg_sz]);
+    regcache_cooked_write (regcache, i + r0_regnum,
+			   &argslotsbuf[i * gpr_reg_sz]);
 
   sp = k1_frame_align (gdbarch, sp);
   len = argslotsnb - params_regs;
   if (len > 0)
-  {
-    sp -= len * gpr_reg_sz;
-    write_memory (sp, argslotsbuf +  params_regs * gpr_reg_sz, len * gpr_reg_sz);
-  }
+    {
+      sp -= len * gpr_reg_sz;
+      write_memory (sp, argslotsbuf + params_regs * gpr_reg_sz,
+		    len * gpr_reg_sz);
+    }
 
   if (struct_return)
     regcache_cooked_write_unsigned (regcache, r0_regnum + 15, struct_addr);
@@ -337,47 +350,52 @@ k1_push_dummy_call (struct gdbarch *gdbarch, struct value *function, struct regc
 }
 
 static void
-k1_store_return_value (struct gdbarch *gdbarch, struct type *type, struct regcache *regcache, const gdb_byte *buf)
+k1_store_return_value (struct gdbarch *gdbarch, struct type *type,
+		       struct regcache *regcache, const gdb_byte *buf)
 {
   int i, len = TYPE_LENGTH (type);
-  int r0_regnum = user_reg_map_name_to_regnum (get_regcache_arch (regcache), "r0", -1);
+  int r0_regnum
+    = user_reg_map_name_to_regnum (get_regcache_arch (regcache), "r0", -1);
   int sz = register_size (gdbarch, r0_regnum);
 
   for (i = 0; len > sz; i++, len -= sz)
     regcache_raw_write (regcache, i + r0_regnum, buf + i * sz);
 
   if (len > 0)
-  {
-    gdb_byte tmp[sz];
+    {
+      gdb_byte tmp[sz];
 
-    memset (tmp, 0, sz);
-    memcpy (tmp, buf + i * sz, len);
-    regcache_raw_write (regcache, i + r0_regnum, tmp);
-  }
+      memset (tmp, 0, sz);
+      memcpy (tmp, buf + i * sz, len);
+      regcache_raw_write (regcache, i + r0_regnum, tmp);
+    }
 }
 
 static void
-k1_extract_return_value (struct gdbarch *gdbarch, struct type *type, struct regcache *regcache, gdb_byte *buf)
+k1_extract_return_value (struct gdbarch *gdbarch, struct type *type,
+			 struct regcache *regcache, gdb_byte *buf)
 {
   int i, len = TYPE_LENGTH (type);
-  int r0_regnum = user_reg_map_name_to_regnum (get_regcache_arch (regcache), "r0", -1);
+  int r0_regnum
+    = user_reg_map_name_to_regnum (get_regcache_arch (regcache), "r0", -1);
   int sz = register_size (gdbarch, r0_regnum);
 
   for (i = 0; len > sz; i++, len -= sz)
     regcache_raw_read (regcache, i + r0_regnum, buf + i * sz);
 
   if (len > 0)
-  {
-    gdb_byte tmp[sz];
+    {
+      gdb_byte tmp[sz];
 
-    regcache_raw_read (regcache, i + r0_regnum, tmp);
-    memcpy (buf + i * sz, tmp, len);
-  }
+      regcache_raw_read (regcache, i + r0_regnum, tmp);
+      memcpy (buf + i * sz, tmp, len);
+    }
 }
 
 enum return_value_convention
-k1_return_value (struct gdbarch *gdbarch, struct value *func_type, struct type *type,
-  struct regcache *regcache, gdb_byte *readbuf, const gdb_byte *writebuf)
+k1_return_value (struct gdbarch *gdbarch, struct value *func_type,
+		 struct type *type, struct regcache *regcache,
+		 gdb_byte *readbuf, const gdb_byte *writebuf)
 {
   if (TYPE_LENGTH (type) > 32)
     return RETURN_VALUE_STRUCT_CONVENTION;
@@ -398,9 +416,11 @@ k1_get_longjmp_target (struct frame_info *frame, CORE_ADDR *pc)
   struct gdbarch *gdbarch = get_frame_arch (frame);
   enum bfd_endian byte_order = gdbarch_byte_order (gdbarch);
 
-  get_frame_register (frame, user_reg_map_name_to_regnum (get_frame_arch (frame), "r0", -1), buf);
+  get_frame_register (
+    frame, user_reg_map_name_to_regnum (get_frame_arch (frame), "r0", -1), buf);
   r0 = extract_unsigned_integer (buf, sizeof (buf), byte_order);
-  if (target_read_memory (r0 + 0xA0, buf, sizeof (buf))) // 0xA0 = offset of RA in the jmp_buf struct
+  // 0xA0 = offset of RA in the jmp_buf struct
+  if (target_read_memory (r0 + 0xA0, buf,sizeof (buf)))
     return 0;
 
   *pc = extract_unsigned_integer (buf, sizeof (buf), byte_order);
@@ -424,62 +444,67 @@ k1_look_for_insns (void)
   int i;
 
   for (i = 0; i < K1_NUM_ARCHES; ++i)
-  {
-    k1opc_t *op;
-
-    switch (i)
     {
-      case K1_K1C: op = k1c_k1optab;
-        break;
-      default:
-        internal_error (__FILE__, __LINE__, "Unknown arch id.");
-    }
+      k1opc_t *op;
 
-    while (op->as_op[0])
-    {
-      if (strcmp ("call", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("icall", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("goto", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("igoto", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("ret", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("rfe", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("pcrel", op->as_op) == 0)
-        add_op (&pcrel_insn[i], op);
-      else if (strcmp ("scall", op->as_op) == 0)
-      {
-        if (op->format && op->format[0] && op->format[0]->regs == NULL)
-        {
-          break_jtag_over_iss[i] = (op->codewords[0].opcode & op->codewords[0].mask) |
-            ((__NR_BREAKPOINT_JTAGISS & ((1 << op->format[0]->bfield->size) - 1))
-            << op->format[0]->bfield->to_offset);
-          break_op[i] = (op->codewords[0].opcode & op->codewords[0].mask) |
-            ((__NR_BREAKPOINT_PL0 & ((1 << op->format[0]->bfield->size) - 1))
-            << op->format[0]->bfield->to_offset);
-        }
-        add_op (&branch_insns[i], op);
-      }
-      else if (strncmp ("cb.", op->as_op, 3) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("loopdo", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("loopgtz", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("loopnez", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("get", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("iget", op->as_op) == 0)
-        add_op (&branch_insns[i], op);
-      else if (strcmp ("nop", op->as_op) == 0)
-        nop_op[i] = op->codewords[0].opcode;
+      switch (i)
+	{
+	case K1_K1C:
+	  op = k1c_k1optab;
+	  break;
+	default:
+	  internal_error (__FILE__, __LINE__, "Unknown arch id.");
+	}
 
-      ++op;
+      while (op->as_op[0])
+	{
+	  if (strcmp ("call", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("icall", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("goto", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("igoto", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("ret", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("rfe", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("pcrel", op->as_op) == 0)
+	    add_op (&pcrel_insn[i], op);
+	  else if (strcmp ("scall", op->as_op) == 0)
+	    {
+	      if (op->format && op->format[0] && op->format[0]->regs == NULL)
+		{
+		  break_jtag_over_iss[i]
+		    = (op->codewords[0].opcode & op->codewords[0].mask)
+		      | ((__NR_BREAKPOINT_JTAGISS
+			  & ((1 << op->format[0]->bfield->size) - 1))
+			 << op->format[0]->bfield->to_offset);
+		  break_op[i]
+		    = (op->codewords[0].opcode & op->codewords[0].mask)
+		      | ((__NR_BREAKPOINT_PL0
+			  & ((1 << op->format[0]->bfield->size) - 1))
+			 << op->format[0]->bfield->to_offset);
+		}
+	      add_op (&branch_insns[i], op);
+	    }
+	  else if (strncmp ("cb.", op->as_op, 3) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("loopdo", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("loopgtz", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("loopnez", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("get", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("iget", op->as_op) == 0)
+	    add_op (&branch_insns[i], op);
+	  else if (strcmp ("nop", op->as_op) == 0)
+	    nop_op[i] = op->codewords[0].opcode;
+
+	  ++op;
+	}
     }
-  }
 }
