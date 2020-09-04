669931ac6db980e65eb36f7fef99c97e8b579ae1
Marius Gligor <mgligor@kalray.eu>
2020-09-04 18:29:12 +0200

    Search prologue/epilogue instructions for the KVX frame unwinder
    Summary:
    In the current implementation, the backtrace stops when it reaches a frame
    without debug information because gdb cannot compute the previous frame
    registers (e.g. program counter, stack pointer). This commit searches for
    instructions specific to prologue and epilogue
    
    	*opcodes/kv3-dis.c (decode_prologue_epilogue_bundle): New function to
    	extract instructions useful for the frame unwinding from a bundle
    	*opcodes/kv3-dis.h: New file containing functions and data types
    	declarations used by the decode_prologue_epilogue_bundle function
    
    Ref T14298
    
    Test Plan: gdb should be able now to bactrace frames not containing debug information
    
    Reviewers: O12 gdb!
    
    Maniphest Tasks: T14298
    
    Differential Revision: https://phab.kalray.eu/D8158

---
+++ opcodes/kv3-dis.h
@@ -0,0 +1,43 @@
+#ifndef _KV3_DIS_H_
+#define _KV3_DIS_H_
+
+#include "dis-asm.h"
+
+#define KVX_GPR_REG_SP 12
+#define KVX_GPR_REG_FP 14
+
+enum kvx_prologue_epilogue_insn_type
+{
+  KVX_PROL_EPIL_INSN_SD,
+  KVX_PROL_EPIL_INSN_SQ,
+  KVX_PROL_EPIL_INSN_SO,
+  KVX_PROL_EPIL_INSN_GET_RA,
+  KVX_PROL_EPIL_INSN_ADD_FP,
+  KVX_PROL_EPIL_INSN_ADD_SP,
+  KVX_PROL_EPIL_INSN_RESTORE_SP_FROM_FP,
+  KVX_PROL_EPIL_INSN_GOTO,
+  KVX_PROL_EPIL_INSN_IGOTO,
+  KVX_PROL_EPIL_INSN_CB,
+  KVX_PROL_EPIL_INSN_RET,
+  KVX_PROL_EPIL_INSN_CALL,
+};
+
+struct kvx_prologue_epilogue_insn
+{
+  enum kvx_prologue_epilogue_insn_type insn_type;
+  unsigned long long immediate;
+  int gpr_reg[3];
+  int nb_gprs;
+};
+
+struct kvx_prologue_epilogue_bundle
+{
+  struct kvx_prologue_epilogue_insn insn[6];
+  int nb_insn;
+};
+
+int decode_prologue_epilogue_bundle (bfd_vma memaddr,
+				     struct disassemble_info *info,
+				     struct kvx_prologue_epilogue_bundle *pb);
+
+#endif
