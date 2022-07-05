bb2536d7ee8a4bea13f550aa4c12de6e5627cbc6
Benoit Dinechin <benoit.dinechin@kalray.eu>
2022-07-05 13:58:37 +0200

    Replace unused signed6 by wrapped8 immediate.
    Summary:
    Immediate_signed6 is unused and Immediate_wrapped8 is needed for the v2
    coprocessor.
    
    Test Plan: CI
    
    Reviewers: jvillette, O12 gdb!
    
    Differential Revision: https://phab.kalray.eu/D21985

--- gas/config/tc-kvx.c
+++ gas/config/tc-kvx.c
@@ -1261,7 +1261,7 @@ match_operands(const kv3opc_t * op, const expressionS * tok,
 
             case Immediate_kv3_pcrel17:
             case Immediate_kv3_pcrel27:
-            case Immediate_kv3_signed6:
+            case Immediate_kv3_wrapped8:
             case Immediate_kv3_signed10:
             case Immediate_kv3_signed16:
             case Immediate_kv3_signed27:
@@ -2036,7 +2036,7 @@ insn_syntax(kv3opc_t *op, char *buf, int buf_size)
       break;
     case Immediate_kv3_pcrel17:
     case Immediate_kv3_pcrel27:
-    case Immediate_kv3_signed6:
+    case Immediate_kv3_wrapped8:
     case Immediate_kv3_signed10:
     case Immediate_kv3_signed16:
     case Immediate_kv3_signed27:
